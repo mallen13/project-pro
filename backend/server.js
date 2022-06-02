50dda02371425059e4e3435402cdcc494146490d
= <=>
2022-06-02 18:21:03 -0400

    achieved 100% test coverage

--- backend/server.js
+++ backend/server.js
@@ -24,78 +24,80 @@ let lists = [
 ]
 
 //api status
-app.get('/api/status', (req,res) => res.json('API is working! :)') )
+app.get('/list-app/api/status', (req,res) => res.json('API is working! :)') )
 
 //get lists
-app.get('/api/get-lists', (req, res) => {
-    //post to db
-    //send appropriate response
+app.get('/list-app/api/get-lists', (req, res) => {
+    //if not lists
     res.status(200).json({lists: lists})
-    console.log('list sent');
+    console.log('lists sent');
 })
 
 //create list
-app.post('/api/create-list', (req, res) => {
+app.post('/list-app/api/create-list', (req, res) => {
     const { listTitle } = req.body;
 
-    //send appropriate response
+    //post to dv
     if (lists.map( list => list.title).includes(listTitle)) {
       res.status(409).json('list already exists');
       return;
     }
-
-    //post to db
     lists.push({
       title: listTitle,
       items: []
     })
 
     res.status(200).json('success');
-    console.log('list created');
+    console.log(`"${listTitle}" created`);
 })
 
 //delete list
-app.post('/api/delete-list', (req,res) => {
+app.post('/list-app/api/delete-list', (req,res) => {
   const { listTitle } = req.body;
-  console.log(listTitle);
   
-  //post to db
+  //if list doesn't exist
+
+  //remove from db
   const index = lists.map(obj => obj.title).indexOf(listTitle); 
   if (index === -1) lists = [];
   else lists.splice(index,1);
 
   //send response
-  console.log('list deleted');
+  console.log(`"${listTitle}" deleted`);
   res.status(200).json('success');
 })
 
 //add list item
-app.post('/api/add-list-item', (req,res) => {
+app.post('/list-app/api/add-list-item', (req,res) => {
   const { listTitle,listItem } = req.body;
 
+  //post to db
   const index = lists.map(obj => obj.title).indexOf(listTitle); 
-
   if (lists[index].items.includes(listItem)) {
     res.status(409).json('list item already exists');
     return;
   }
-
   lists[index].items.push(listItem);
+
   res.status(200).json('success');
-  console.log('list item added');
+  console.log(`"${listItem}" added to list "${listTitle}"`);
 })
 
 //delete list items
-app.post('/api/delete-list-item', (req,res) => {
+app.post('/list-app/api/delete-list-item', (req,res) => {
   const { listTitle,listItem } = req.body;
 
+  //if item doesn't exists
+
+  //remove from db
   const listIndex = lists.map(obj => obj.title).indexOf(listTitle); 
   const itemIndex = lists[listIndex].items.indexOf(listItem);
   lists[listIndex].items.splice(itemIndex,1);
   res.status(200).json('success');
-  console.log('list item removed');
+  console.log(`list item "${listItem}" removed from list "${listTitle}"`);
 })
 
+//listen
 app.listen(port, () => {
   console.log(`List App backend listening on port ${port}`)
 })
